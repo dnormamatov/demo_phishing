069ad59b768f9b86ce9eeb3bb494e6b5380b9757
Dilshodbek <dnormamatov570@gmail.com>
2026-06-19 23:12:49 +0500

    Test route

--- app.py
+++ app.py
@@ -1,25 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("login.html")
-
-@app.route("/login", methods=["POST"])
-def login():
-    print("LOGIN ROUTE HIT")
-
-    username = request.form.get("username")
-    password = request.form.get("password")
-
-    print("Username:", username)
-    print("Password:", password)
-
-    return """
-    <h2>Form received</h2>
-    <a href="/">Back</a>
-    """
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+def home():
+    return "<h1>Render ishlayapti!</h1>"
