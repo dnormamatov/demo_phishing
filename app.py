dbb21fe7501dd4f0018e74f50f325835f937ebdb
Dilshodbek <dnormamatov570@gmail.com>
2026-06-19 23:14:35 +0500

    Restore demo app

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "<h1>Render ishlayapti!</h1>"
+def index():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    print("LOGIN ROUTE HIT")
+
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    print("Username:", username)
+    print("Password:", password)
+
+    return """
+    <h2>Form received</h2>
+    <a href="/">Back</a>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
